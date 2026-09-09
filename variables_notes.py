99d102aebed13e35a6c1f0ed902745d492fb51e4
Purple-Cable14 <caleb.king@ucas-edu.net>
2026-09-09 14:52:01 -0600

    Variables Notes

---
+++ variables_notes.py
@@ -0,0 +1,7 @@
+# CK, Variables Notes <= for people to read, not the computer
+
+name = input("Tell me your name: ")
+
+print("Hello there.")
+print("I like turtles!")
+print("Hello",name, "Welcome to class.")
