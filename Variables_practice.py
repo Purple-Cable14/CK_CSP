34d3f8950724c49c10e0b9fb85a14c2ca4abfe67
Purple-Cable14 <caleb.king@ucas-edu.net>
2026-09-10 14:31:31 -0600

    Variables_practice

---
+++ Variables_practice.py
@@ -0,0 +1,41 @@
+# CK, Variables Practice
+
+my_name = "Caleb"
+
+one_digit_number = "7"
+
+three_digit_number = "282"
+
+breakfast_meal = "German Pancakes"
+
+favorite_color = "purple"
+
+school_name = "Utah County Academy of Sciences"
+
+current_year = "2026"
+
+my_eye_color = "blue"
+
+my_age = "14"
+
+favorite_school_subject = "Math"
+
+print(my_name)
+
+print(one_digit_number)
+
+print(three_digit_number)
+
+print(breakfast_meal)
+
+print(favorite_color)
+
+print(school_name)
+
+print(current_year)
+
+print(my_eye_color)
+
+print(my_age)
+
+print(favorite_school_subject)
