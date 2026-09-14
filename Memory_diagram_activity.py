ba94dcb8c560a3d23a5b819278d2984281b107cd
Purple-Cable14 <caleb.king@ucas-edu.net>
2026-09-14 14:54:33 -0600

    Memory diagram activity

---
+++ Memory_diagram_activity.py
@@ -0,0 +1,37 @@
+# CK, Memory Diagram Activity
+
+name = "Caleb"
+
+print(f"The variable name has a value of {name} and is saved at {id(name)}")
+
+age = 14
+
+print(f"The variable age has a value of {age} and is saved at {id(age)}")
+
+score = 10
+
+print(f"The variable score has a value of {score} and is saved at {id(score)}")
+
+score = 25
+
+print(f"The variable score has a value of {score} and is saved at {id(score)}")
+
+height = 4.11
+
+print(f"The variable height has a value of {height} and is saved at {id(height)}")
+
+city = "Orem"
+
+print(f"The variable city has a value of {city} and is saved at {id(city)}")
+
+zip_code = 80202
+
+print(f"The variable zip_code has a value of {zip_code} and is saved at {id(zip_code)}")
+
+price1 = 10
+
+print(f"The variable price1 has a value of {price1} and is saved at {id(price1)}")
+
+price2 = 10
+
+print(f"The variable price2 has a value of {price2} and is saved at {id(price2)}")
