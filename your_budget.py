d0d22db82b0c5c626086893f47861d970d8fee53
Purple-Cable14 <caleb.king@ucas-edu.net>
2026-09-17 14:52:48 -0600

    Your Budget

---
+++ your_budget.py
@@ -0,0 +1,37 @@
+# CK, Your Budget
+
+while True:
+    try:
+        income=float(input("What is your monthly income: $"))
+        break
+    except:
+        print("That is not a number.")
+
+while True:
+    try:
+        rent=float(input("What is your monthly rent/mortgage: $"))
+        break
+    except:
+        print("That is not a number.")
+
+while True:
+    try:
+        utilities=float(input("What is your monthly utilities: $"))
+        break
+    except:
+        print("That is not a number.")
+
+while True:
+    try:
+        groceries=float(input("What is your monthly groceries: $"))
+        break
+    except:
+        print("That is not a number.")
+
+while True:
+    try:
+        transportation=float(input("What is your monthly transportation: $"))
+        break
+    except:
+        print("That is not a number.")
+
