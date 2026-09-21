6f4967a764d33a61326ebd11349ff8af7032d9be
Purple-Cable14 <caleb.king@ucas-edu.net>
2026-09-21 14:52:26 -0600

    Silly Sentences

---
+++ silly_sentences.py
@@ -0,0 +1,33 @@
+# CK, Silly Sentences
+
+adjective = input("Tell me an adjective: ").strip().lower()
+
+nationality = input("Tell me an nationality: ").strip().capitalize()
+
+person = input("Tell me a person's name: ").strip().capitalize()
+
+noun = input("Tell me a noun: ").strip().lower()
+
+adjective2 = input("Tell me another adjective: ").strip().lower()
+
+noun2 = input("Tell me another noun: ").strip().lower()
+
+adjective3 = input("Tell me another adjective: ").strip().lower()
+
+adjective4 = input("Tell me another adjective: ").strip().lower()
+
+plural_noun = input("Tell me a plural noun: ").strip().lower()
+
+noun3 = input("Tell me another noun: ").strip().lower()
+
+number = input("Tell me a number: ").strip()
+
+shape = input("Tell me a shape, but add an S at the end: ").strip().lower()
+
+food = input("Tell me a food: ").strip().lower()
+
+food2 = input("Tell me another food: ").strip().lower()
+
+number2 = input("Tell me another number: ").strip()
+
+print("Pizza was invented by a "+adjective+" "+nationality+" chef named "+person+". To make a pizza, you needto take a lump of "+noun+", and make a thin, round "+adjective2+" "+noun2+". Then you cover it with "+adjective3+" sauce, "+adjective4+" cheese, and fresh chopped "+plural_noun+". Next you have to bake it in a very hot "+noun3+". When it is done, cut it into "+number+" "+shape+". Some kids like "+food+" pizza the best, but my favorite is the "+food2+" pizza. If I could, I would eat pizza "+number2+" times a day!")
