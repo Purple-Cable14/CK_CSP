e618de8cc81ab086d9da4f5e92548e5f6021392b
Purple-Cable14 <caleb.king@ucas-edu.net>
2026-09-25 01:44:35 +0000

    Password Strength Checker

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -13,7 +13,7 @@ symbol = False
 password = input("What is your password: ").strip()
 
 for letter in password:
-    if (len(password)) >= 8:
+    if len(password) >= 8:
         length = True
 
 for letter in password:
@@ -32,6 +32,30 @@ for letter in password:
     if letter in "!@#$%^&*-_<>?":
         symbol = True
 
+score = 0
+
+if length:
+    score += 1
+
+if uppercase:
+    score += 1
+
+if lowercase:
+    score += 1
+
+if number:
+    score += 1
+
+if symbol:
+    score += 1
+
+if score == 5:
+    strength = "Strong"
+elif score >= 3:
+    strength = "Medium"
+else:
+    strength = "Weak"
+
 print(f"At least 8 characters: {length}")
 
 print(f"Has an uppercase letter: {uppercase}")
@@ -42,5 +66,22 @@ print(f"Has a number: {number}")
 
 print(f"Has a symbol: {symbol}")
 
-score = 0
-if (len(password)) >= 8:
+print(f"Strength: {strength}")
+
+if strength != "Strong":
+    print("You are missing:")
+
+    if length == False:
+        print("At least 8 characters,")
+
+    if uppercase == False:
+        print("At least one uppercase letter,")
+
+    if lowercase == False:
+        print("At least one lowercase letter,")
+
+    if number == False:
+        print("At least one number")
+
+    if symbol == False:
+        print("At least one symbol")
