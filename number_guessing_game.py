2f044af6c076552022dd145c97ad8d9fe34615c5
Purple-Cable14 <caleb.king@ucas-edu.net>
2026-09-25 14:53:03 -0600

    Number Guessing Game

---
+++ number_guessing_game.py
@@ -0,0 +1,9 @@
+# CK, Number Guessing Game
+import random
+
+print("I'm thinking of a number between 1 and 100. You have 6 tries to guess it! ")
+
+number = random.randint(1,101)
+
+guess = input("What is your guess: ")
+
