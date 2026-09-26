8067b10c6bbc39845959b2da12e23146be654040
Purple-Cable14 <caleb.king@ucas-edu.net>
2026-09-26 00:06:47 +0000

    Number Guessing Game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,12 +3,21 @@ import random
 
 number = random.randint(1,101)
 
-guesses = 6
+number_of_guesses = 1
 
 print("I'm thinking of a number between 1 and 100. You have 6 tries to guess it! ")
 
 while True:
-    name
-
-guess = input("What is your guess: ")
-
+    guesses = int(input(f"Guess number {number_of_guesses}: "))
+    if guesses == number:
+        print("You Win!")
+        break
+    elif number_of_guesses == 6:
+        print(f"You Lose! The number was {number}!")
+        break
+    elif guesses > number:
+        print("The number is lower.")
+        number_of_guesses += 1
+    else:
+        print ("The number is higher.")
+        number_of_guesses += 1
